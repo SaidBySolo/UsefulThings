629655f35764eb490016df7d0bd1bdd024444fd1
ombe1229 <h3236516@gmail.com>
2021-02-01 23:29:45 +0900

    경고 제거

--- html2py.py
+++ html2py.py
@@ -2,6 +2,6 @@ from bs4 import BeautifulSoup
 
 
 def html2text(html):
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, 'html.parser')
     text_parts = soup.findAll(text=True)
     return ''.join(text_parts)
